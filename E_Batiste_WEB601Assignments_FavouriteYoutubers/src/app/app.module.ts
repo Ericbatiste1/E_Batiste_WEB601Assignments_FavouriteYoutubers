12103456eedd3981d052eac709d21e23e7bc3062
eric <90633675+Ericbatiste1@users.noreply.github.com>
2022-02-20 21:35:01 -0500

    added hover-affect directive

--- E_Batiste_WEB601Assignments_FavouriteYoutubers/src/app/app.module.ts
+++ E_Batiste_WEB601Assignments_FavouriteYoutubers/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ContentListComponent } from './content-list/content-list.component';
 import { ContentPipePipe } from './content-pipe.pipe';
+import { HoverAffectDirective } from './hover-affect.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     ContentListComponent,
-    ContentPipePipe
+    ContentPipePipe,
+    HoverAffectDirective
   ],
   imports: [
     BrowserModule
