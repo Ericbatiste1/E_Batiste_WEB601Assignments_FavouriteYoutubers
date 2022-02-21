d05cbe2e50473c2fbbc004353b7dace8332b2e88
eric <90633675+Ericbatiste1@users.noreply.github.com>
2022-02-20 22:20:09 -0500

    hover affect applied to type and tags

--- E_Batiste_WEB601Assignments_FavouriteYoutubers/src/app/hover-affect.directive.ts
+++ E_Batiste_WEB601Assignments_FavouriteYoutubers/src/app/hover-affect.directive.ts
@@ -1,10 +1,23 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, Input, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appHoverAffect]'
 })
 export class HoverAffectDirective {
+  @Input() styleEffect?: string;
 
-  constructor() { }
+  constructor(private elm: ElementRef) { 
+    this.elm.nativeElement.style.cursor = "pointer";
+  }
 
+  @HostListener('mouseover') hovered(typeElm: any) {
+    this.elm.nativeElement.style.textDecoration = this.styleEffect ?? "none";
+    this.elm.nativeElement.style.fontWeight = this.styleEffect ?? "normal";
+  }
+
+  @HostListener('mouseout') notHovered(typeElm: any) {
+    this.elm.nativeElement.style.textDecoration = "none";
+    this.elm.nativeElement.style.fontWeight = "normal";
+  }
+  
 }
